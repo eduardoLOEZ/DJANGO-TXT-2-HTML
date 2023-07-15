d491f5e56b77b4c1ea48384758c4a0e18d9ef65d
Eduardo Loeza <84247208+eduardoLOEZ@users.noreply.github.com>
2023-07-14 21:41:34 -0600

    Delete DJANGO-TXT-HTML directory

--- DJANGO-TXT-HTML/converter/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from ckeditor.fields import RichTextField
-
-
-# Create your models here.
-
-class Editor(models.Model):
-    body = RichTextField(blank=True, null= True)
